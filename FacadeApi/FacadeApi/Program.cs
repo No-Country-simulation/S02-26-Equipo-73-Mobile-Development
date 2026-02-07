2e19e8fda65257474a1444dc8baa2de9c0935c30
Exequiel65 <marcos97britos@gmail.com>
2026-02-07 00:00:38 -0300

    add migrations

--- FacadeApi/FacadeApi/Program.cs
+++ FacadeApi/FacadeApi/Program.cs
@@ -1,4 +1,6 @@
+using Infrastructure.Context;
 using Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,31 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+
+        // Aplica las migraciones si hay cambios pendientes
+        if ((await context.Database.GetPendingMigrationsAsync()).Any())
+        {
+            Console.WriteLine("Applying pending migrations...");
+            await context.Database.MigrateAsync();
+            Console.WriteLine("Migrations applied successfully.");
+        }
+        else
+        {
+            Console.WriteLine("No pending migrations.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error applying migrations: {ex.Message}");
+        throw;
+    }
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
