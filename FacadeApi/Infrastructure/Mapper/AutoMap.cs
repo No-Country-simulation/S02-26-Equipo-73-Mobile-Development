e547141a29ee2b5c731924e902c8ab1c72ccbf4b
Exequiel65 <marcos97britos@gmail.com>
2026-02-11 20:43:27 -0300

    Implementación de AutoMapper en ProductRepository
    Se reemplazó el mapeo manual por perfiles de AutoMapper, optimizando consultas con ProjectTo y reduciendo el código repetitivo (~30%). El repositorio ahora recibe IMapper por inyección de dependencias. Se añadieron documentos explicativos sobre la configuración, ventajas y mejores prácticas de AutoMapper. Mejora la mantenibilidad, performance y testabilidad del proyecto.

--- FacadeApi/Infrastructure/Mapper/AutoMap.cs
+++ FacadeApi/Infrastructure/Mapper/AutoMap.cs
@@ -1,12 +1,33 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Application.DTOs.Products;
+using Domain.Entities.Products;
 
 namespace Infrastructure.Mapper
 {
     public class AutoMap : Profile
     {
-        public AutoMap() { }
+        public AutoMap() 
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.BrandName, opt => opt.MapFrom(src => src.Brand.Name))
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
+                .ForMember(dest => dest.Variants, opt => opt.MapFrom(src => src.Variants));
+
+            CreateMap<ProductVariant, ProductVariantDto>()
+                .ForMember(dest => dest.SizeLabel, opt => opt.MapFrom(src => src.BrandSize.Label));
+
+            CreateMap<CreateProductDto, Product>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true))
+                .ForMember(dest => dest.Brand, opt => opt.Ignore())
+                .ForMember(dest => dest.Category, opt => opt.Ignore())
+                .ForMember(dest => dest.Variants, opt => opt.Ignore());
+
+            CreateMap<UpdateProductDto, Product>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Brand, opt => opt.Ignore())
+                .ForMember(dest => dest.Category, opt => opt.Ignore())
+                .ForMember(dest => dest.Variants, opt => opt.Ignore());
+        }
     }
 }
